9ea5e0156f2ea8fd45ce4bbb3b1050ad97b843dc
David Nash <david.allyxanddar.nash@gmail.com>
2015-08-25 18:37:14 -0400

    argh

--- band/app/controllers/events_controller.rb
+++ band/app/controllers/events_controller.rb
@@ -7,6 +7,21 @@ class EventsController < ApplicationController
 		
 	end
 
+	def new
+		@venue = Venue.find(params[:venue_id])
+		@bands = Band.all
+		@event = Event.new
+	end
 
+	# def create
+	# 	@venue = Venue.find(params[:venue_id])
+	# 	@event = Event.create(event_params)
+	# 	redirect_to event_path(@event)
+	# end
+
+	# private
+	# 	def params
+	# 		params.require(:event).permit(:date, :alcohol, :band_id, :venue_id)
+	# 	end
 
 end
